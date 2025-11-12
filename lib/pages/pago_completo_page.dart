65531cab67b5c1865aef991a7e1c406925f1cda6
Yader Campbell <yadermejiac123@gmail.com>
2025-11-11 22:58:24 -0600

    feat: Basic definition of the project

---
+++ lib/pages/pago_completo_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PagoCompletoPage extends StatelessWidget {
+  const PagoCompletoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
