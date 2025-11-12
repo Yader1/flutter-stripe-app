5f99228735eecaee5a07afbdb7106f957d9b8407
Yader Campbell <yadermejiac123@gmail.com>
2025-11-11 23:21:37 -0600

    feat: home screen design, cards

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+import '../data/tarjetas.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('Pagar'),
+        actions: [
+          IconButton(
+            onPressed: (){
+
+            }, 
+            icon: Icon(Icons.add)
+          )
+        ],
       ),
-      body: const Center(
-        child: Text('Welcome to the Home Page!'),
-      ),
+      body: Stack(
+        children: [
+          Positioned(
+            width: size.width,
+            height: size.height,
+            top: 200,
+            child: PageView.builder(
+              controller: PageController(
+                viewportFraction: 0.9
+              ),
+              physics: BouncingScrollPhysics(),
+              itemCount: tarjetas.length,
+              itemBuilder: (_, i) {
+                var tarjeta = tarjetas[i];
+            
+                return CreditCardWidget(
+                  cardNumber: tarjeta.cardNumberHidden, 
+                  expiryDate: tarjeta.expiracyDate, 
+                  cardHolderName: tarjeta.cardHolderName, 
+                  cvvCode: tarjeta.cvv, 
+                  showBackView: false, 
+                  onCreditCardWidgetChange: (credit){
+            
+                  }
+                );
+              }
+            ),
+          ),
+        ],
+      )
     );
   }
 }
